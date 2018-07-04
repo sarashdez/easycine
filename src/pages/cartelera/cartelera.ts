3394ef79df1bb73364327c21bd7e9f0d1c58946f
Sara Suarez <sara.shdez@gmail.com>
2018-07-04 17:45:09 +0200

    Parametros busqueda y mapa

--- src/pages/cartelera/cartelera.ts
+++ src/pages/cartelera/cartelera.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, NavParams} from 'ionic-angular';
 import { DetallePage } from '../detalle/detalle';
 import { TrailerPage } from '../trailer/trailer';
 import { ComprarEntradasPage } from '../comprar-entradas/comprar-entradas';
@@ -12,7 +12,24 @@ import { ViewController } from "ionic-angular";
 })
 export class CarteleraPage {
 
-  constructor(public navCtrl: NavController, private viewCtrl: ViewController) {
+  empresa : string;
+  dia : string;
+  hora : string;
+  cercania : string;
+
+  constructor(public navCtrl: NavController,
+              public param: NavParams,
+              private viewCtrl: ViewController) {
+    this.empresa = param.get("empresa");
+    this.dia = param.get("dia");
+    this.hora = param.get("hora");
+    this.cercania = param.get("cercania");
+
+    console.log("Parametros recuperados");
+    console.log("Empresa: " + this.empresa);
+    console.log("Dia: " + this.dia);
+    console.log("Hora: " + this.hora);
+    console.log("Cercania: " + this.cercania);
   }
 
   ionViewWillEnter() {
--- src/pages/criterios-busqueda/criterios-busqueda.ts
+++ src/pages/criterios-busqueda/criterios-busqueda.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { CarteleraPage } from '../cartelera/cartelera';
-import { DetallePage } from '../detalle/detalle';
-import { TrailerPage } from '../trailer/trailer';
-import { ComprarEntradasPage } from '../comprar-entradas/comprar-entradas';
-import { PaypalPage } from '../paypal/paypal';
-import { EligeTuCinePage } from '../elige-tu-cine/elige-tu-cine';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import * as moment from 'moment';
 
 @Component({
   selector: 'page-criterios-busqueda',
@@ -13,25 +10,47 @@ import { EligeTuCinePage } from '../elige-tu-cine/elige-tu-cine';
 })
 export class CriteriosBusquedaPage {
 
-  constructor(public navCtrl: NavController) {
+  public form: FormGroup;
+  public date : any;
+
+  constructor(public navCtrl: NavController,
+              private _FB : FormBuilder,
+  ) {
+    this.form = this._FB.group({
+      'empresa' : [''],
+      'dia' : [''],
+      'hora' : [''],
+      'cercania' : ['']
+    })
+  };
+
+  goToCartelera(){
+    let empresa: string = this.form.controls['empresa'].value;
+    let dia: string = this.form.controls['dia'].value;
+    let hora: string = this.form.controls['hora'].value;
+    let cercania : string = this.form.controls['cercania'].value;
+
+    //Formatear fecha
+    let diaFormateado = moment(dia).format("DD/MM/YYYY");
+
+
+    console.log("Valores obtenidos del formulario");
+    console.log("Empresa: " + empresa);
+    console.log("Dia: " + dia);
+    console.log("Dia formateado: " + diaFormateado);
+    console.log("Hora: " + hora);
+    console.log("Cercania: " + cercania);
+
+    this.navCtrl.push(CarteleraPage, {
+      empresa : empresa,
+      dia : diaFormateado,
+      hora : hora,
+      cercania : cercania
+    });
   }
-  goToCartelera(params){
-    if (!params) params = {};
-    this.navCtrl.push(CarteleraPage);
-  }goToDetalle(params){
-    if (!params) params = {};
-    this.navCtrl.push(DetallePage);
-  }goToTrailer(params){
-    if (!params) params = {};
-    this.navCtrl.push(TrailerPage);
-  }goToComprarEntradas(params){
-    if (!params) params = {};
-    this.navCtrl.push(ComprarEntradasPage);
-  }goToPaypal(params){
-    if (!params) params = {};
-    this.navCtrl.push(PaypalPage);
-  }goToEligeTuCine(params){
-    if (!params) params = {};
-    this.navCtrl.push(EligeTuCinePage);
-  }
+
+
+
+
+
 }
