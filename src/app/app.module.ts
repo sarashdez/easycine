7df187292749a4ec6c31c92cadbdc613ffdd6a2f
Sara Suárez <sara.shdez@gmail.com>
2019-01-04 22:20:10 +0000

    Elige tu cine eliminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,8 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { MyApp } from './app.component';
-
 import { HomePage } from '../pages/home/home';
 import { CriteriosBusquedaPage } from '../pages/criterios-busqueda/criterios-busqueda';
-import { EligeTuCinePage } from '../pages/elige-tu-cine/elige-tu-cine';
 import { CarteleraPage } from '../pages/cartelera/cartelera';
 import { DetallePage } from '../pages/detalle/detalle';
 import { ProximosEstrenosPage } from '../pages/proximos-estrenos/proximos-estrenos';
@@ -17,19 +15,16 @@ import { ComprarEntradasPage } from '../pages/comprar-entradas/comprar-entradas'
 import { MiPerfilPage } from '../pages/mi-perfil/mi-perfil';
 import { LoginPage } from '../pages/login/login';
 import { CrearCuentaPage } from '../pages/crear-cuenta/crear-cuenta';
-
 import { DatePicker } from "@ionic-native/date-picker";
 import { Camera } from '@ionic-native/camera';
 import { ImagePicker } from '@ionic-native/image-picker';
 import { environment } from "../environments/environment";
-
 import { AngularFireModule } from "angularfire2";
 import { AngularFireAuthModule } from "angularfire2/auth";
 import { AutenticacionProvider } from "../providers/autenticacion/autenticacion";
 import { AngularFireStorageModule } from "angularfire2/storage";
 import { StorageProvider } from "../providers/storage/storage";
 import { AngularFirestoreModule } from "angularfire2/firestore";
-
 import { YoutubeVideoPlayer } from "@ionic-native/youtube-video-player";
 import { PayPal } from "@ionic-native/paypal";
 
@@ -39,7 +34,6 @@ import { PayPal } from "@ionic-native/paypal";
     MyApp,
     HomePage,
     CriteriosBusquedaPage,
-    EligeTuCinePage,
     CarteleraPage,
     DetallePage,
     ProximosEstrenosPage,
@@ -63,7 +57,6 @@ import { PayPal } from "@ionic-native/paypal";
     MyApp,
     HomePage,
     CriteriosBusquedaPage,
-    EligeTuCinePage,
     CarteleraPage,
     DetallePage,
     ProximosEstrenosPage,
