1a1ea407eba82a97cf5b94015a6e412f1923dc0c
Sara Suárez <sara.shdez@gmail.com>
2019-01-05 16:55:57 +0000

    Side menu refactorizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Platform, Nav } from 'ionic-angular';
+import {Platform, Nav, AlertController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { CarteleraPage } from '../pages/cartelera/cartelera';
@@ -11,6 +11,7 @@ import { HomePage } from '../pages/home/home';
 import {MiPerfilPage} from "../pages/mi-perfil/mi-perfil";
 import {AngularFireAuth} from "angularfire2/auth";
 import * as firebase from "firebase";
+import {DetallePage} from "../pages/detalle/detalle";
 
 
 @Component({
@@ -24,7 +25,8 @@ export class MyApp {
   constructor(platform: Platform,
               statusBar: StatusBar,
               splashScreen: SplashScreen,
-              private _ANGFIRE: AngularFireAuth) {
+              private _ANGFIRE: AngularFireAuth,
+              private alertCtrl: AlertController) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
@@ -55,31 +57,14 @@ export class MyApp {
     this.navCtrl.setRoot(ProximosEstrenosPage);
   }
 
-  goToMisEntradas(params){
-    if (!params) params = {};
-    this.navCtrl.setRoot(MisEntradasPage);
+  goToMisEntradas(sesion : string){
+    this.navCtrl.push(MisEntradasPage, {
+      sesion: sesion});
   }
 
   goToCriteriosBusqueda(params){
     if (!params) params = {};
     this.navCtrl.setRoot(CriteriosBusquedaPage);
-  }
-
-  comprobarUsuarioLoggedPerfil() {
-    //Primero se comprueba si el usuario ha iniciado sesion. Si es asi, se muestra su info. Si no se redirige a Login.
-    let uid : string;
-    console.log("Comprobar usuario logued");
-    this._ANGFIRE.authState.subscribe(session => {
-      if(session) {
-        //Usuario logueado
-        uid = session.uid;
-        console.log("Usuario logueado: "+uid);
-        this.goToMiPerfil();
-      } else {
-        console.log("Ningun usuario logueado");
-        this.goToLogin();
-      }
-    });
   }
 
   goToMiPerfil(){
@@ -89,6 +74,63 @@ export class MyApp {
   goToLogin(){
     this.navCtrl.push(LoginPage);
   }
+
+  comprobarUsuarioLoggedPerfil() {
+    //Primero se comprueba si el usuario ha iniciado sesion. Si es asi, se muestra su info. Si no se redirige a Login.
+    let uid : string;
+    this._ANGFIRE.authState.subscribe(session => {
+      if(session) {
+        //Usuario logueado
+        uid = session.uid;
+        this.goToMiPerfil();
+      } else {
+        this.goToLogin();
+      }
+    });
+  }
+
+  comprobarUsuarioLoggedMisEntradas() {
+    //Primero se comprueba si el usuario ha iniciado sesion. Si es asi, se muestran sus entradas. Si no se redirige un mensaje.
+    let uid : string;
+    this._ANGFIRE.authState.subscribe(session => {
+      if(session) {
+        //Usuario logueado
+        uid = session.uid;
+        this.goToMisEntradas(uid);
+      } else {
+        this.alertaSesionNoIniciada();
+      }
+    });
+  }
+
+  /**
+   * Alerta que informa al usuario de que no ha iniciado sesion. Puede elegir que se le
+   * rediriga a Login o continuar usando la aplicacion.
+   */
+  alertaSesionNoIniciada() {
+    let confirm = this.alertCtrl.create({
+      title: 'Sesión no iniciada',
+      message: 'Esta opción solo está disponible para usuarios registrados. ¿Te gustaría iniciar sesión?',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            console.log('"No" pulsado');
+          }
+        },
+        {
+          text: 'Sí',
+          handler: () => {
+            console.log('"Sí" pulsado');
+            this.goToLogin();
+          }
+        }
+      ]
+    });
+    confirm.present();
+  }
+
+
 
 
 
